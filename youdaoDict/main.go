e0ecc50fa78da192e2ddb2d5c6a2515d725027e7
h1k0n <ybzjyhk@gmail.com>
2019-09-03 18:23:31 +0800

    Update main.go
    read from txt

--- youdaoDict/main.go
+++ youdaoDict/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+        "bufio"
         "encoding/xml"
         "fmt"
+        "io"
+        "io/ioutil"
+        "log"
+        "os"
+        "strings"
 )
 
 type Wordbook struct {
-        Items []WordItem `xml:"wordbook"`
+        XMLName xml.Name `xml:"wordbook"`
+        Items   []WordItem
 }
 type WordItem struct {
         XMLName  xml.Name `xml:"item"`
@@ -14,7 +21,7 @@ type WordItem struct {
         Trans    CDATA    `xml:"trans"`
         Phonetic CDATA    `xml:"phonetic"`
         Tags     string   `xml:"tags"`
-        Progress string   `xml:"progress"`
+        Progress int      `xml:"progress"`
 }
 type CDATA string
 
@@ -25,13 +32,38 @@ func (c CDATA) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 }
 
 func main() {
-        var a WordItem
-        a.Trans = "aa"
-
         var wb Wordbook
 
-        wb.Items = append(wb.Items, a)
+        f, err := os.Open("import.txt")
+        if err != nil {
+                panic(err)
+        }
+        defer f.Close()
+
+        rd := bufio.NewReader(f)
+        for {
+                line, err := rd.ReadString('\n') //以'\n'为结束符读入一行
+
+                if err != nil || io.EOF == err {
+                        break
+                }
+                line = strings.TrimSpace(line)
+                if resSlice := strings.Split(line, "|"); len(resSlice) >= 2 {
+
+                        var a WordItem
+                        a.Word = resSlice[0]
+                        a.Trans = CDATA(resSlice[1])
+                        a.Tags = "rustlangtrans"
+                        a.Progress = -1
+                        wb.Items = append(wb.Items, a)
+                }
+
+        }
         xmlOutPut, _ := xml.MarshalIndent(&wb, "", "  ")
         fmt.Println(string(xmlOutPut))
+        err = ioutil.WriteFile("output.xml", xmlOutPut, 0644)
+        if err != nil {
+                log.Fatal(err)
+        }
 
 }
